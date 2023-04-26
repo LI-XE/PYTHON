4c36a62ee5f9c599722befd9e08baaecec32dfd2
LI_XE <hyangeun312@gmail.com>
2023-04-25 21:41:01 -0500

    Bank Account

--- python/fundamentals/oop/main.py
+++ python/fundamentals/oop/main.py
@@ -61,19 +61,104 @@
 # print(Kevin_shoe.cut_price_by(10))
 
 
-# 3
-class Player:
-    def __init__(self, name, age, position, team):
+# # 3
+# class Player:
+#     def __init__(self, name, age, position, team):
+#         self.name = name
+#         self.age = age
+#         self.position = position
+#         self.team = team
+
+
+# # Uncomment the line below to test
+# player_kevin = Player("kevin Durant", 34, "small forward", "Brooklyn Nets")
+
+
+4.
+# Bank Account
+
+
+class User:
+    def __init__(self, name, email):
         self.name = name
-        self.age = age
-        self.position = position
-        self.team = team
+        self.email = email
+        self.accounts = [BankAccount(int_rate=0.02, balance=0), BankAccount(
+            int_rate=0.02, balance=0)]
+        for account_number in range(len(self.accounts)):
+            if account_number:
+                self.account = self.accounts[account_number - 1]
 
-    def print_Player(self):
-        print(f'{self.name} = {"name": {self.name}, "age": {self.age}, "position": {self.position}, "team": {self.team}}')
+    def deposit(self, account_number, ammount):
+        if account_number:
+            self.accounts[account_number-1].balance += ammount
+            return self
+
+    def withdrawal(self, account_number, amount):
+        if account_number:
+            if amount <= self.account.balance:
+                self.accounts[account_number-1].balance -= amount
+                return self
+            else:
+                print(
+                    f"Please check your account {account_number} balance.")
+        return self
+
+    def display_balance(self, account_number):
+        if account_number:
+            print(
+                f"User: {self.name}, account{account_number} Balance: ${self.accounts[account_number - 1].balance}")
+            return self
+
+    def transfer_money(self, account_number, other_user, amount):
+        if account_number:
+            if self.account.balance >= amount:
+                self.accounts[account_number - 1].balance -= amount
+            else:
+                print(f"Please check your account {account_number} balance.")
+            other_user.account.balance += amount
+        return self
+
+    def self_transfer_money(self, account_number1, account_number2, amount):
+        if account_number1:
+            if self.account.balance >= amount:
+                self.account.balance -= amount
+            else:
+                print(f"Please check your account {account_number1} balance.")
+            self.account_number2 = account_number2
+            self.accounts[account_number2 - 1].balance += amount
+        return self
 
 
-# Uncomment the line below to test
-player_kevin = Player("kevin Durant", 34, "small forward", "Brooklyn Nets")
-print(player_kevin.name)
-print(player_kevin.print_Player())
+class BankAccount:
+    def __init__(self, int_rate=0.02, balance=0):
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print("Please check your balance first.")
+
+    def display_account_info(self):
+        print(f"Balance: ${self.balance}")
+
+    def yield_interest(self, int_rate=0.05):
+        if self.balance > 0:
+            self.balance += self.balance * self.int_rate
+            return self
+
+
+Jason = User("Jason", "jason@gmail.com")
+Kevin = User("Kevin", "kevin@gmail.com")
+Jack = User("Jack", "jack@gmail.com")
+
+Jason.deposit(1, 500).withdrawal(1, 100).self_transfer_money(
+    1, 2, 200).display_balance(1)
+Jason.deposit(2, 300).withdrawal(
+    2, 100).transfer_money(2, Kevin, 50).display_balance(2)
+Kevin.display_balance(1)
