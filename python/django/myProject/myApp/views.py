dca25ecb091beef215b6f52ced1d6ce95284e3e5
LI_XE <hyangeun312@gmail.com>
2023-05-10 23:57:13 -0500

    pratice for models views urls

--- python/django/myProject/myApp/views.py
+++ python/django/myProject/myApp/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render, redirect
+from .models import Shoe
 
 # Create your views here.
 
 
 def index(request):
     return render(request, "home.html")
+
+
+def new(request):
+    context = {
+        "all_shoes": Shoe.objects.all()
+    }
+    return render(request, "show.html", context)
+
+
+def create(request):
+    if "has_laces" not in request.POST:
+        Shoe.objects.create(brand=request.POST["brand"], color=request.POST["color"],
+                            material=request.POST["material"], has_laces=False, count=request.POST["count"])
+    else:
+        Shoe.objects.create(brand=request.POST["brand"], color=request.POST["color"],
+                            material=request.POST["material"], has_laces=request.POST["has_laces"], count=request.POST["count"])
+
+    return redirect("/new")
