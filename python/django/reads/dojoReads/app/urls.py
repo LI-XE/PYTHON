859f43e8c1e2885d660c819040f3a3bcdbe18293
LI_XE <hyangeun312@gmail.com>
2023-06-13 00:49:07 -0500

    added delete review btn

--- python/django/reads/dojoReads/app/urls.py
+++ python/django/reads/dojoReads/app/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path('register', views.register),
     path('login', views.login),
     path('logout', views.logout),
-    # path('books', views.),
+    path('books', views.dashboard),
     path('books/add', views.add_book),
     path('books/<int:book_id>', views.book_detail),
     path('books/<int:book_id>/add/review', views.book_detail),
+    path('books/<int:book_id>/delete/<int:review_id>', views.review_delete),
+    # path('users/<int:user_id>', views.user_page),
 ]
